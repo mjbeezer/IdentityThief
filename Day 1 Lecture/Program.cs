8333e1b74b8978a32cfb5c3495e4af63bce5aba2
Matt B <mjbeezer11621@gmail.com>
2021-09-27 13:36:07 -0400

    first commit

---
+++ Day 1 Lecture/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day_1_Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to the identity stealer!");
+
+            Console.WriteLine("Can you give me your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Can you give me your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Can you give me your hometown?");
+            string homeTown = Console.ReadLine();
+
+            //Console.WriteLine("Your name is  " + name + ". Your age is " + age + ". Your hometown is " + homeTown + ".");
+            Console.WriteLine($"Your name is {name}. Your age is {age}. Your hometown is {homeTown}.");
+        }
+    }
+}
